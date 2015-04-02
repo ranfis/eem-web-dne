7b7a277f5577fa2eb78accaf8a7def17ec3b4562
ranfis <ranfis.sh@gmail.com>
2015-04-01 23:19:14 -0400

    Update offers form

--- client/app/offers/offers.controller.js
+++ client/app/offers/offers.controller.js
@@ -1,6 +1,21 @@
 'use strict';
 
 angular.module('eemApp')
-  .controller('OffersCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('OffersCtrl', function ($scope, $http, socket) {
+    $scope.types = [];
+    $scope.newOffer = {};
+
+    $http.get('/api/types').success(function(types) {
+      $scope.types = types;
+      socket.syncUpdates('thing', $scope.types);
+    });
+
+    $scope.addOffer = function() {
+      var expirationDate = $("#txtExpirationDate").val();//TODO: This is a bug. Fix ASAP. PD: Blame bootstrap-datetimepicker
+      var realDate = new Date(expirationDate);
+      $scope.newOffer.expirationDate = realDate;
+      $http.post('/api/offers', $scope.newOffer);
+      $scope.newOffer = {};
+    };
+
   });
